8287c7a844e09d5342931a2b9bf9009649007425
decentration <ajramseys@gmail.com>
2021-05-02 17:57:44 +0100

    changes for Edgeware

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,20 +3,21 @@ import { JsonRpcProvider } from '@ethersproject/providers';
 import { Wallet } from '@ethersproject/wallet';
 import { MediaFactory } from '../typechain/MediaFactory';
 import { MarketFactory } from '../typechain/MarketFactory';
+require('dotenv').config();
 
 async function start() {
   const args = require('minimist')(process.argv.slice(2));
 
-  if (!args.chainId) {
-    throw new Error('--chainId chain ID is required');
-  }
-  const path = `${process.cwd()}/.env${
-    args.chainId === 1 ? '.prod' : args.chainId === 4 ? '.dev' : '.local'
-  }`;
-  await require('dotenv').config({ path });
+  // if (!args.chainId) {
+  //   throw new Error('--chainId chain ID is required');
+  // }
+  // const path = `${process.cwd()}/.env${
+  //   args.chainId === 1 ? '.prod' : args.chainId === 4 ? '.dev' : '.local'
+  // }`;
+  // await require('dotenv').config({ path });
   const provider = new JsonRpcProvider(process.env.RPC_ENDPOINT);
   const wallet = new Wallet(`0x${process.env.PRIVATE_KEY}`, provider);
-  const sharedAddressPath = `${process.cwd()}/addresses/${args.chainId}.json`;
+  const sharedAddressPath = `${process.cwd()}/addresses/${process.env.CHAIN_ID}.json`;
   // @ts-ignore
   const addressBook = JSON.parse(await fs.readFileSync(sharedAddressPath));
   if (addressBook.market) {
@@ -53,8 +54,11 @@ async function start() {
   await tx.wait();
   console.log(`Market configured.`);
 
+  const media = MediaFactory.connect(addressBook.media, wallet);
+  
+
   await fs.writeFile(sharedAddressPath, JSON.stringify(addressBook, null, 2));
-  console.log(`Contracts deployed and configured. ☼☽`);
+  console.log(`New-Futures deployed and configured Zora contracts on Edgeware!`);
 }
 
 start().catch((e: Error) => {
